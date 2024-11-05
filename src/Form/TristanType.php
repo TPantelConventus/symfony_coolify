62117f0c971c891add4e8cb572bcdcc30f17b3e4
tristan <tristan.pantel@conventus.de>
2024-11-05 15:58:00 +0100

    feat: init

--- src/Form/TristanType.php
+++ src/Form/TristanType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Tristan;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +13,8 @@ class TristanType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('lastname')
+            ->add('name', TextType::class)
+            ->add('lastname', TextType::class)
         ;
     }
 
